f51bdb7ef3331cb56bf57d8305ae46dd85e2c152
David Trevisan <david.trevisan@vemd.it>
2025-04-11 19:06:08 +0200

    [.gitignore] Reorganize tracking, and move search_chr file accordingly

--- hdl/msim/msim_options.tcl
+++
@@ -1,6 +0,0 @@
-# This file is supposed to be included by other scripts
-# Include simulation and run options here, such as messages suppression
-
-# Suppress std arith warnings
-set StdArithNoWarnings 1
-set NumericStdNoWarnings 1
