7896db3476fd8c87d60bd42428c74f5d6bdb28ce
David Trevisan <david.trevisan@vemd.it>
2025-05-02 11:46:37 +0200

    [search_chr] WIP improved

--- projects/02_search_chr/c_improved/code/TB.vhdl
+++ projects/02_search_chr/c_improved/code/TB.vhdl
@@ -21,61 +21,11 @@ architecture behav of tb is
     type tb_statetype is (INIT, TEST1, WAIT1, FINISHED1, FINISHED2, FINISHED3);
     signal tb_state, tb_nextstate   : tb_statetype;
 
-    -- Parameterized array size
-    constant MAX_INSTR               : integer := 3;
-    type array_of_integers           is array (0 to MAX_INSTR - 1) of integer;
-    type array_of_arrays_of_integers is array (0 to 2) of array_of_integers;
+    type array_of_integers           is array (natural range <>) of integer;
 
-    -- Function to load instructions from file
-    impure function load_instr return array_of_arrays_of_integers is
-        file instr_file     : text;
-        variable fstatus    : file_open_status;
-        variable inputline  : line;
-        variable instr_buffer : array_of_arrays_of_integers;
-        variable i          : integer := 0;
-        variable addr_val, char_val, len_val : integer;
-    begin
-        file_open(fstatus, instr_file, "../assets/instr.txt", READ_MODE);
-        if fstatus = OPEN_OK then
-            while (i < MAX_INSTR and not endfile(instr_file)) loop
-                -- Read three consecutive values (ADDRESS, CHAR, LEN)
-                readline(instr_file, inputline);
-                read(inputline, addr_val);
-
-                readline(instr_file, inputline);
-                read(inputline, char_val);
-
-                readline(instr_file, inputline);
-                read(inputline, len_val);
-
-                report "Read values - ADDRESS: " & integer'image(addr_val) &
-                ", CHAR: " & integer'image(char_val) &
-                ", LEN: " & integer'image(len_val);
-
-                -- Store in the intermediate array (0: address, 1: char, 2: length)
-                instr_buffer(0)(i) := (addr_val);  -- ADDRESSES
-                instr_buffer(1)(i) := (char_val);  -- CHARS
-                instr_buffer(2)(i) := (len_val);   -- LENS
-
-                -- Read empty line
-                readline(instr_file, inputline);
-
-                i := i + 1;
-            end loop;
-            file_close(instr_file);
-        else
-            report "load_instr: ERROR, can't open instr.txt";
-        end if;
-        return instr_buffer;
-    end function;
-
-    -- Load data into separate arrays
-    signal instr_data : array_of_arrays_of_integers := load_instr;
-
-    -- Unpack into separate signals for simulation
-    signal ADDRESSES : array_of_integers;
-    signal CHARS     : array_of_integers;
-    signal LENS      : array_of_integers;
+    signal ADDRESSES    : array_of_integers(0 to 2) := ( 3,  5, 20);
+    signal CHARS        : array_of_integers(0 to 2) := ( 3,  3,  5);
+    signal LENS         : array_of_integers(0 to 2) := (10, 10, 15);
 
     signal START                    : std_logic;
     signal ADDRESS                  : std_logic_vector(31 downto 0);
@@ -94,29 +44,25 @@ architecture behav of tb is
 
 begin
     start_process: process
-        begin
-            report "Generic parameter check: MEM_LAT = " & integer'image(MEM_LAT);
+    begin
+        report "Generic parameter check: MEM_LAT = " & integer'image(MEM_LAT);
 
-            rst_n <= '1' , '0' after 1 ns , '1' after 199 ns;
-            started <= true;
-
-            ADDRESSES <= instr_data(0);
-            CHARS     <= instr_data(1);
-            LENS      <= instr_data(2);
-            wait;
-        end process;
+        rst_n <= '1' , '0' after 1 ns , '1' after 199 ns;
+        started <= true;
+        wait;
+    end process;
 
     clkgen: process
-        begin
-            clk <= '1' , '0' after 5 ns;
-            wait for 10 ns;
+    begin
+        clk <= '1' , '0' after 5 ns;
+        wait for 10 ns;
 
-            if end_simul then
-                wait;
-            else
-                nsimul_cycles <= nsimul_cycles + 1 ;
-            end if;
-        end process;
+        if end_simul then
+            wait;
+        else
+            nsimul_cycles <= nsimul_cycles + 1 ;
+        end if;
+    end process;
 
     DUT : entity work.search_chr
         port map (
@@ -166,7 +112,7 @@ begin
 
             when WAIT1 =>
                 if READY = '1' then
-                    if cnt = MAX_INSTR then
+                    if cnt = ADDRESSES'length then
                         tb_nextstate <= FINISHED1;
                     else
                         tb_nextstate <= TEST1;
