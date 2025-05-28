ceaa992173ee8c45c1712f0b35f6f9153381fb01
David Trevisan <david.trevisan@vemd.it>
2025-05-28 22:33:28 +0200

    [scripts] updates on sync and 03 device xsim

--- projects/03_multiplier_and_clz/scripts/xsim.tcl
+++ projects/03_multiplier_and_clz/scripts/xsim.tcl
@@ -1,17 +1,25 @@
+# Prepare VCD file (signals trace)
 database -open testbenchvcd -into testbench.vcd -vcd
 probe -all -database testbenchvcd -depth all
 database -open devicevcd -into device.vcd -vcd
 probe -all -database devicevcd -depth all DUT
+# Prepare TRN file for simvision
 database -open testbenchtrn -into testbench.trn -default
 probe -all -database testbenchtrn -depth all
 database -open devicetrn -into device.trn
 probe -all -database devicetrn -depth all DUT
+
+# Prepare SAIF file for switching activity
 dumpsaif -depth all -output device.saif -scope DUT -internal
 
+# Run simulation
 run
 
+# Close db files
 database -close tstbenchvcd
 database -close devicevcd
 database -close testbenchtrn
 database -close devicetrn
+
+# Exit
 exit
