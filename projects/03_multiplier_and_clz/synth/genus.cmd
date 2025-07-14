8b0310ba0d5b66cc7495749fe90e34cc47741b7a
David Trevisan <david.trevisan@vemd.it>
2025-07-14 13:39:42 +0200

    [deploy] add synth results

---
+++ projects/03_multiplier_and_clz/synth/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 19.14-s108_1, built Jul  7 2020 16:22:44
+
+# Date: Mon Jul 14 13:32:37 2025
+# Host: eda-27267 (x86_64 w/Linux 5.10.0-9-amd64) (1core*1cpu*1physical cpu*QEMU Virtual CPU version 2.5+ 16384KB)
+# OS:   Unsupported OS as /etc does not have release info
+
+source /home/david.trevisan/03_multiplier_and_clz/scripts/genus.tcl
---
+++ projects/04_mcd/synth/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 19.14-s108_1, built Jul  7 2020 16:22:44
+
+# Date: Thu Jul 10 15:30:06 2025
+# Host: eda-27267 (x86_64 w/Linux 5.10.0-9-amd64) (1core*1cpu*1physical cpu*QEMU Virtual CPU version 2.5+ 16384KB)
+# OS:   Unsupported OS as /etc does not have release info
+
+source /home/david.trevisan/04_mcd/scripts/genus.tcl
